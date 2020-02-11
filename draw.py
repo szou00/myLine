1567eaab45f19751e96a19394ebb66ef03bc8521
Sharon Zou <szou00@stuy.edu>
2020-02-11 05:55:30 -0500

    changed

--- draw.py
+++ draw.py
@@ -7,17 +7,16 @@ def draw_line( x0, y0, x1, y1, screen, color ):
     if x0 > x1:
         x0, x1, y0, y1 = x1, x0, y1, y0
 
-    current_x = x0
-    current_y = y0
-
     A = y1 - y0
     B = -1 * (x1 - x0)
 
+    current_x, current_y = x0, y0
     if current_x < x1:
         m = -1 * float(A) / float(B)
         #if in octant 1
         if m <= 1 and m >= 0:
-            d = (2 * A) + B
+            d = 2 * A + B
+            current_x, current_y = x0, y0
             while current_x <= x1:
                 plot(screen, color, current_x, current_y)
                 #if it's above the midpoint
@@ -28,7 +27,8 @@ def draw_line( x0, y0, x1, y1, screen, color ):
                 d += 2 * A
 
         elif m > 1:
-            d = A + (2 * B)
+            d = A + 2 * B
+            current_x, current_y = x0, y0
             while current_y <= y1:
                 plot(screen, color, current_x, current_y)
                 #if it's below the midpoint
@@ -39,7 +39,8 @@ def draw_line( x0, y0, x1, y1, screen, color ):
                 d += 2 * B
 
         elif m <= 0 and m >= -1:
-            d = (2 * A) + B
+            d = 2 * A + B
+            current_x, current_y = x0, y0
             while current_x <= x1:
                 plot(screen, color, current_x, current_y)
                 if d < 0:
@@ -49,7 +50,8 @@ def draw_line( x0, y0, x1, y1, screen, color ):
                 d += 2 * A
 
         elif m <= -1:
-            d = A - (2 * B)
+            d = A - 2 * B
+            current_x, current_y = x0, y0
             while current_y >= y1:
                 plot(screen, color, current_x, current_y)
                 if d > 0:
@@ -59,6 +61,7 @@ def draw_line( x0, y0, x1, y1, screen, color ):
                 d -= 2 * B
 
     else:
+        current_x, current_y = x0, y0
         if current_y < y1:
             while current_y < y1:
                 plot(screen, color, current_x, current_y)
